a73799073dcb6a890000b6287eb2ce52912271b2
Antti Launiainen <antti.launiainen@suomi24.fi>
2016-03-26 18:10:27 -0400

    Support for groups formatted as datatables

--- typescript/Tinycar/Ui/Component/Group.ts
+++ typescript/Tinycar/Ui/Component/Group.ts
@@ -13,8 +13,22 @@ module Tinycar.Ui.Component
 		// Build group components
 		private buildComponents():void
 		{
+			// Default container
+			var container = this.htmlContent;
+			
+			// Number of columns to use
+			var columns = this.Model.getNumber('columns');
+			
+			// Create first row
+			if (columns > 0)
+			{
+				container = $('<div>').
+					attr('class', 'row').
+					appendTo(this.htmlContent);
+			}
+			
 			// Create components only for this tab
-			this.Model.get('components').forEach((item:Object) =>
+			this.Model.get('components').forEach((item:Object, index:number) =>
 			{
 				// Create new model instance
 				let model = new Tinycar.Model.DataItem(item);
@@ -30,12 +44,32 @@ module Tinycar.Ui.Component
 					this.App, this.View, model
 				);
 				
+				// Add new columns container
+				if (columns > 0 && index % columns === 0)
+				{
+					container = $('<div>').
+						attr('class', 'row').
+						appendTo(this.htmlContent);
+				}
+				
 				// Build to components list
-				this.htmlContent.append(instance.build());
+				container.append(instance.build());
 				
 				// Add to list of components
 				this.View.addComponent(instance);
 			});
+		}
+		
+		// @see Tinycar.Main.Field.getRootStyles()
+		getRootStyles():Array<string>
+		{
+			let result = super.getRootStyles();
+			
+			// Group format style
+			result.push('format-' + this.Model.get('format'));
+			result.push('columns-' + this.Model.get('columns'));
+			
+			return result;
 		}
 	}
 }
