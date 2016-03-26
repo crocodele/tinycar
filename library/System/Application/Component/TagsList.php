0e253e53d7a06a732fc852f2d33499c3d45202ab
Antti Launiainen <antti.launiainen@suomi24.fi>
2016-03-26 19:46:20 -0400

    Fixes #1

--- library/System/Application/Component/TagsList.php
+++ library/System/Application/Component/TagsList.php
@@ -1,15 +1,15 @@
 <?php
-	
+
 	namespace Tinycar\System\Application\Component;
 
 	use Tinycar\Core\Http\Params;
 	use Tinycar\System\Application\Component;
 	use Tinycar\System\Application\View\Field;
-	
+
 	class TagsList extends Field
 	{
-		
-		
+
+
 		/**
 		 * @see Tinycar\System\Application\View\Field::onModelAction()
 		 */
@@ -21,7 +21,15 @@
 			$result['placeholder'] = $this->getStringValue(
 				$this->xdata->getString('placeholder')
 			);
-			
+
+			// Change direct value into an array
+			if (is_string($result['data_value']))
+				$result['data_value'] = array($result['data_value']);
+
+			// Enforce array type
+			if (!is_array($result['data_value']))
+				$result['data_value'] = array();
+
 			return $result;
 		}
 	}
