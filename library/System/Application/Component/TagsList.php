eca53189a152338d1b216e05dfd25b5060c32ea5
Antti Launiainen <antti.launiainen@suomi24.fi>
2016-03-27 08:47:45 -0400

    Change data value format in retrieval method instead of model structure

--- library/System/Application/Component/TagsList.php
+++ library/System/Application/Component/TagsList.php
@@ -11,6 +11,26 @@
 
 
 		/**
+		 * @see Tinycar\System\Application\View\Field::getDataValue()
+		 */
+		public function getDataValue($default = null)
+		{
+			// Get data value
+			$result = parent::getDataValue($default);
+
+			// Change direct value into an array
+			if (is_string($result))
+				$result = array($result);
+
+			// Enforce array type
+			if (!is_array($result))
+				$result = array();
+
+			return $result;
+		}
+
+
+		/**
 		 * @see Tinycar\System\Application\View\Field::onModelAction()
 		 */
 		public function onModelAction(Params $params)
@@ -21,14 +41,6 @@
 			$result['placeholder'] = $this->getStringValue(
 				$this->xdata->getString('placeholder')
 			);
-
-			// Change direct value into an array
-			if (is_string($result['data_value']))
-				$result['data_value'] = array($result['data_value']);
-
-			// Enforce array type
-			if (!is_array($result['data_value']))
-				$result['data_value'] = array();
 
 			return $result;
 		}
