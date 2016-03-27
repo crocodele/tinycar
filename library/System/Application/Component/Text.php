5d2332d1b2517b5a25c316981ab8baf3eabad82c
Antti Launiainen <antti.launiainen@suomi24.fi>
2016-03-27 10:39:42 -0400

    Added support for text have a link

--- library/System/Application/Component/Text.php
+++ library/System/Application/Component/Text.php
@@ -1,25 +1,47 @@
 <?php
-	
+
 	namespace Tinycar\System\Application\Component;
 
 	use Tinycar\Core\Http\Params;
 	use Tinycar\System\Application\View\Field;
-	
+
 	class Text extends Field
 	{
-		
+
+		/**
+		 * Get link properties
+		 * @return array link properties in key-value pairs
+		 */
+		private function getLinkProperties()
+		{
+			$result = array();
+
+			// Get attributes
+			$list = $this->xdata->getAttributes('link');
+
+			// Process values
+			foreach ($list as $name => $value)
+				$result[$name] = $this->view->getStringValue($value);
+
+			return $result;
+		}
+
+
 		/**
 		 * @see Tinycar\System\Application\View\Field::onModelAction()
 		 */
 		public function onModelAction(Params $params)
 		{
 			$result = parent::onModelAction($params);
-				
+
 			// Placeholder content
 			$result['placeholder'] = $this->view->getStringValue(
 				$this->xdata->getString('placeholder')
 			);
-				
+
+			// Link properties
+			$result['link'] = $this->getLinkProperties();
+
 			return $result;
 		}
 	}
--- typescript/Tinycar/Ui/Component/Text.ts
+++ typescript/Tinycar/Ui/Component/Text.ts
@@ -7,31 +7,74 @@ module Tinycar.Ui.Component
 		// Build content
 		buildContent()
 		{
-			// Build elements
 			super.buildContent();
 			
 			// Build placeholder
 			if (this.getDataValue() === null)
 				this.buildPlaceholder();
 			
+			// Build link
+			else if (this.Model.isObject('link'))
+				this.buildLink();
+			
 			// Build text content
 			else
 				this.buildText();
 		}
 		
+		// Build link
+		private buildLink():void
+		{
+			// Target URL
+			let url = Tinycar.Url.getAsPath(
+				this.Model.getObject('link')
+			);
+			
+			// Create link container
+			let container = $('<a>').
+				attr('href', url). 
+				attr('class', 'link').
+				appendTo(this.htmlContent);
+			
+			// Add label
+			$('<span>').
+				attr('class', 'label').
+				html(this.getDataValue()).
+				appendTo(container);
+			
+			// Add icon
+			$('<span>').
+				attr('class', 'icon icon-tiny icon-arrow-right').
+				appendTo(container);
+			
+			// When clicked
+			container.click((e:Event) =>
+			{
+				Tinycar.System.Page.setState('unloading');
+			});
+		}
+		
 		// Build placeholder
 		private buildPlaceholder():void
 		{
-			$('<div>').
-				attr('class', 'placeholder').
-				html(this.Model.get('placeholder')).
+			// Get placeholder string
+			let value = this.Model.getString('placeholder');
+			
+			// Use an empty string
+			if (value.length === 0)
+				value = '&nbsp;'
+
+			// Create placeholder container
+			$('<span>').
+				attr('class', 'text placeholder').
+				html(value).
 				appendTo(this.htmlContent);
 		}
 		
 		// Build text content
 		private buildText():void
 		{
-			$('<div>').
+			$('<span>').
 				attr('class', 'text').
 				html(this.getDataValue()).
 				appendTo(this.htmlContent);
