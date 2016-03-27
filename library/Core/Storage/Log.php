70a409ec624e6150155dfabc4823e034349dc8af
Antti Launiainen <antti.launiainen@suomi24.fi>
2016-03-27 08:50:14 -0400

    Changing data output format

--- library/Core/Storage/Log.php
+++ library/Core/Storage/Log.php
@@ -1,13 +1,13 @@
-<?php 
+<?php
 
 	namespace Tinycar\Core\Storage;
-	
+
 	use Tinycar\App\Config;
-	
+
 	class Log
 	{
-		
-		
+
+
 		/**
 		 * Log given data to custom logfile
 		 * @param mixed $arg1 argument #1
@@ -18,14 +18,14 @@
 		{
 			// Get given arguments
 			$args = func_get_args();
-			
+
 			// Resolve message
 			$message = sprintf("%s\n[%s] DEBUG: %s\n",
 				str_repeat('-', 50),
 				date('Y-m-d H:i:s'),
-				var_dump($args, true)
+				print_r($args, true)
 			);
-			
+
 			// Write message to custom logfile
 			error_log($message, 3, Config::getPath(
 				'FOLDER_STORAGE', '/logs/tinycar.log'
