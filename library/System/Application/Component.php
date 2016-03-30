fdea7888e730f199e2c290286448c98696cea680
Antti Launiainen <antti.launiainen@suomi24.fi>
2016-03-30 10:35:33 -0400

    Shorthand for processed node value retrieval

--- library/System/Application/Component.php
+++ library/System/Application/Component.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 	namespace Tinycar\System\Application;
 
@@ -9,7 +9,7 @@
 	use Tinycar\System\Application;
 	use Tinycar\System\Application\View;
 	use Tinycar\System\Application\Model\Property;
-	
+
 	class Component
 	{
 		protected $app;
@@ -23,8 +23,8 @@
 		protected $system;
 		protected $view;
 		protected $xdata;
-		
-		
+
+
 		/**
 		 * Initiate class
 		 * @param object $system Tinycar\App\Manager
@@ -41,8 +41,8 @@
 			$this->id = $id;
 			$this->xdata = $xdata;
 		}
-		
-		
+
+
 		/**
 		 * Load component by type
 		 * @param object $system Tinycar\App\Manager
@@ -55,32 +55,32 @@
 		{
 			// Target type
 			$type = $xdata->getString('@type');
-			
+
 			// Target class name
 			$class_name = 'Tinycar\\System\Application\\Component\\'.$type;
 
 			// Create new instance
 			$instance = (class_exists($class_name) ?
-				new $class_name($system, $app, $view, $id, $xdata) : 
+				new $class_name($system, $app, $view, $id, $xdata) :
 				new self($system, $app, $view, $id, $xdata)
 			);
 
 			return $instance;
 		}
-		
-		
+
+
 		/**
 		 * Call specified component action and return response
 		 * @param string $name target action name
 		 * @param array [$params] custom action data
 		 * @return mixed action response
-		 * @throws Tinycar\Core\Exception 
+		 * @throws Tinycar\Core\Exception
 		 */
 		public function callAction($name, array $params = array())
 		{
 			// Target method name
 			$method = 'on'.$name.'Action';
-			
+
 			// Invalid action name
 			if (!method_exists($this, $method))
 			{
@@ -89,12 +89,12 @@
 					'name' => $name,
 				));
 			}
-			
+
 			// Call action
 			return $this->$method(new Params($params));
 		}
-		
-		
+
+
 		/**
 		 * Get data default value
 		 * @return mixed|null default value or null on failure
@@ -104,18 +104,18 @@
 			// Already resolved
 			if (!is_bool($this->data_default))
 				return $this->data_default;
-					
+
 			// Try to get data property value
 			$result = $this->getStringValue(
 				$this->xdata->getString('data/@default')
 			);
-				
+
 			// Remeber
 			$this->data_default = $result;
 			return $this->data_default;
 		}
-		
-		
+
+
 		/**
 		 * Action to get source data name
 		 * @return string|null name or null on failure
@@ -125,22 +125,22 @@
 			// Already resolved
 			if (!is_bool($this->data_name))
 				return $this->data_name;
-			
+
 			// Try to get service property value
 			$name = $this->getStringValue(
 				$this->xdata->getString('data/@name')
 			);
-			
+
 			// Property intance
 			if ($name instanceof Property)
 				$name = $name->getName();
-			
+
 			// Remember
 			$this->data_name = $name;
 			return $this->data_name;
 		}
-		
-		
+
+
 		/**
 		 * Get data provider source
 		 * @return mixed  value data value
@@ -149,15 +149,15 @@
 		{
 			// Try to get service property value
 			$service = $this->xdata->getString('data/@service');
-			
+
 			// We have a service
 			if (is_string($service))
 				return $service;
 
 			return array();
 		}
-		
-		
+
+
 		/**
 		 * Get data provider type
 		 * @return string type
@@ -167,12 +167,12 @@
 			// We have a service property
 			if (is_string($this->xdata->getString('data/@service')))
 				return 'service';
-			
+
 			// Default
 			return 'static';
 		}
-		
-		
+
+
 		/**
 		 * Get component id
 		 * @return string|null component id or null on failure
@@ -181,8 +181,8 @@
 		{
 			return $this->id;
 		}
-		
-		
+
+
 		/**
 		 * Get model property instance referenced in data
 		 * @return object|null Tinycar\System\Application\Model\Property
@@ -193,14 +193,14 @@
 			// Already resolved
 			if (!is_bool($this->data_property))
 				return $this->data_property;
-					
+
 			// Try to get service property instance
 			$value = $this->getStringValue(
 				$this->xdata->getString('data/@name')
 			);
-			
+
 			// Resolve value
-			$result = ($value instanceof Property ? 
+			$result = ($value instanceof Property ?
 				$value : null
 			);
 
@@ -208,44 +208,64 @@
 			$this->data_property = $result;
 			return $this->data_property;
 		}
-		
-		
+
+
 		/**
 		 * Get active record's property value of for this component
 		 * @param mixed [$default] default value to use instead
-		 * @return mixed|null property value or null on failure 
+		 * @return mixed|null property value or null on failure
 		 */
 		public function getDataValue($default = null)
 		{
 			// Already resolved
 			if ($this->data_value_resolved === true)
 				return $this->data_value;
-			
+
 			// Get active data name
 			$name = $this->getDataName();
-			
+
 			// No data name specified, use default value
 			if (is_null($name))
 				return $default;
-			
+
 			// Get target record instance
 			$record = $this->view->getDataRecord();
-			
+
 			// Get record instance property value
 			$value = $record->get($name);
-			
+
 			// No data available, use default vaule
 			if (is_null($value))
 				return $default;
-			
+
 			// Update state
 			$this->data_value_resolved = true;
 			$this->data_value = $value;
-			
+
 			return $this->data_value;
 		}
-		
-		
+
+
+		/**
+		 * Get string value from specified node path, and process it
+		 * @param string $path target node path
+		 * @param string [$default] target node value if none exists
+		 * @return string|null processed value or null on failure
+		 */
+		public function getNodeString($path, $default = null)
+		{
+			// Get node value
+			$value = $this->xdata->getString($path);
+
+			// No value is available, use default
+			if (is_null($value))
+				$value = $default;
+
+			// Process value
+			return $this->view->getStringValue($value);
+		}
+
+
 		/**
 		 * @see Tinycar\System\Application\View::getStringValue();
 		 */
@@ -253,8 +273,8 @@
 		{
 			return $this->view->getStringValue($source);
 		}
-		
-		
+
+
 		/**
 		 * Get component tab name
 		 * @return string tab name or 'default' on failure
@@ -264,8 +284,8 @@
 			$type = $this->xdata->getString('@tab');
 			return (is_string($type) ? $type : 'default');
 		}
-		
-		
+
+
 		/**
 		 * Get compoennt type label
 		 * @return string|null component label or null on failure
@@ -276,8 +296,8 @@
 				$this->xdata->getString('label')
 			);
 		}
-		
-		
+
+
 		/**
 		 * Get component type name
 		 * @return string|null component type or null on failure
@@ -286,8 +306,8 @@
 		{
 			return $this->xdata->getString('@type');
 		}
-		
-		
+
+
 		/**
 		 * Check if data for this component is required
 		 * @return bool is required
@@ -297,16 +317,16 @@
 			// Already resolved
 			if (is_bool($this->data_required))
 				return $this->data_required;
-			
+
 			$result = false;
-			
+
 			// Get required property
 			$required = $this->xdata->getString('data/@required');
-			
+
 			// We have a valid value
 			if ($required === 'true' || $required === 'false')
 				$result = ($required === 'true');
-			
+
 			// Try to get value from referenced property
 			else
 			{
@@ -317,40 +337,41 @@
 				if (is_object($property))
 					$result = $property->isRequired();
 			}
-			
+
 			// Remember
 			$this->data_required = $result;
 			return $this->data_required;
 		}
-		
-		
+
+
 		/**
 		 * Initiate component for use
 		 */
 		public function init()
 		{
 		}
-		
-		
+
+
 		/**
 		 * Action to get source data
 		 * @param object $params Tinycar\Core\Http\Params instance
-		 * @return array source data 
+		 * @return array source data
 		 */
 		public function onDataAction(Params $params)
 		{
 			// Set fixed properties
 			$params->set('app', $this->app->getId());
-			
+			$params->set('row', $this->view->getDataRecord()->get('id'));
+
 			// Get source data from service
 			$result = $this->app->callService(
 				$this->getDataSource(), $params->getAll()
 			);
-			
+
 			return $result;
 		}
-		
-		
+
+
 		/**
 		 * Action to get custom model data
 		 * @param object $params Tinycar\Core\Http\Params instance
@@ -369,8 +390,8 @@
 				'tab_name'      => $this->getTabName(),
 			);
 		}
-		
-		
+
+
 		/**
 		 * Set new fixed data value for this component
 		 * @param mixed $data new data
@@ -381,4 +402,3 @@
 			$this->data_value = $value;
 		}
 	}
-	
