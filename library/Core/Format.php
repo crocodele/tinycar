c66339c1186b6b6ef90691afb6d48f937f483dfe
Antti Launiainen <antti.launiainen@suomi24.fi>
2016-04-14 10:00:54 -0400

    Adding default app colors to system manifest, modeling for system manifest instance

---
+++ library/Core/Format.php
@@ -0,0 +1,35 @@
+<?php
+
+	namespace Tinycar\Core;
+
+
+	class Format
+	{
+
+
+		/**
+		 * Get color adjustment
+		 * @param string $color source color
+		 * @param int $adjust adjustment color (-255 - +255)
+		 * @return string new color
+		 */
+		public static function adjustColor($color, $adjust)
+		{
+			// Trim value
+			$color = substr($color, 1);
+
+			$result = '#';
+
+			// Adjust R, G and B
+			foreach (str_split($color, 2) as $c)
+			{
+				$c = hexdec($c);
+				$c = max(0, min(255, $c + $adjust));
+				$c = dechex($c);
+
+				$result .= str_pad($c, 2, '0', STR_PAD_LEFT);
+			}
+
+			return $result;
+		}
+	}
