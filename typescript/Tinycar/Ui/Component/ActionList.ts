5ece760e1bd614a489f2e947c5829d01a41ebcd3
Antti Launiainen <antti.launiainen@suomi24.fi>
2016-04-27 10:23:45 -0400

    Show focus rectangle only with keyboard access

--- typescript/Tinycar/Ui/Component/ActionList.ts
+++ typescript/Tinycar/Ui/Component/ActionList.ts
@@ -1,208 +1,208 @@
 module Tinycar.Ui.Component
 {
-	export class ActionList extends Tinycar.Main.Component
-	{
-	
-	
-		// Build content
-		buildContent()
-		{
-			// Build elements
-			super.buildContent();
-			
-			// Add title
-			if (this.Model.hasString('title'))
-			    this.buildTitle();
-			
-			// Build list options
-			this.buildList();
-		}
-		
-		// Build textarea field
-		private buildList():void
-		{
-			// Create options
-			this.Model.getList('options').forEach((item:Object) =>
-			{
-				this.htmlRoot.append(this.buildListItem(item));
-			});
-		}
-		
-		// Build list items
-		private buildListItem(item:Object):JQuery
-		{
-			// Create model instance from data
-			let model = new Tinycar.Model.DataItem(item);
-			
-			// Create container
-			let container = this.buildListItemContainer(model);
-			
-			// Build icon and label
-			container.append(this.buildListItemIcon(model));
-			container.append(this.buildListItemLabel(model));
-			
-			// When clicked
-			container.click((e:JQueryMouseEventObject) =>
-			{
-				// Trigger custom action
-				if (this.triggerListItemAction(model))
-				{
-					e.preventDefault();
-				}
-				// Open fixed URL
-				else if (model.isObject('link_path'))
-				{
-					if (e.ctrlKey === false && e.shiftKey === false)
-					{
-						e.preventDefault();
-						Tinycar.Url.openUrl(container.attr('href'));
-						container.blur();
-					}
-				}
-			});
-			
-			
-			// This action is for saving, let's add a keyboard
-			// short listener to trigger it
-			if (model.get('type') === 'save') 
-			{
-				Tinycar.System.addEvent('ctrl+s', () => 
-				{
-					container.trigger('click');
-				});
-			}
-			
-			return container;
-		}
-		
-		// Build list item container
-		private buildListItemContainer(model:Tinycar.Model.DataItem):JQuery
-		{
-			// Create container
-			let container = $('<a>').
-				attr('class', 'item').
-				attr('tabindex', 0);
-		
-			// Add link to internal path
+    export class ActionList extends Tinycar.Main.Component
+    {
+    
+    
+        // Build content
+        buildContent()
+        {
+            // Build elements
+            super.buildContent();
+            
+            // Add title
+            if (this.Model.hasString('title'))
+                this.buildTitle();
+            
+            // Build list options
+            this.buildList();
+        }
+        
+        // Build textarea field
+        private buildList():void
+        {
+            // Create options
+            this.Model.getList('options').forEach((item:Object) =>
+            {
+                this.htmlRoot.append(this.buildListItem(item));
+            });
+        }
+        
+        // Build list items
+        private buildListItem(item:Object):JQuery
+        {
+            // Create model instance from data
+            let model = new Tinycar.Model.DataItem(item);
+            
+            // Create container
+            let container = this.buildListItemContainer(model);
+            
+            // Build icon and label
+            container.append(this.buildListItemIcon(model));
+            container.append(this.buildListItemLabel(model));
+            
+            // When clicked
+            container.click((e:JQueryMouseEventObject) =>
+            {
+                // Trigger custom action
+                if (this.triggerListItemAction(model))
+                {
+                    e.preventDefault();
+                    container.blur();
+                }
+                // Open fixed URL
+                else if (model.isObject('link_path'))
+                {
+                    if (e.ctrlKey === false && e.shiftKey === false)
+                    {
+                        e.preventDefault();
+                        Tinycar.Url.openUrl(container.attr('href'));
+                        container.blur();
+                    }
+                }
+            });
+            
+            // This action is for saving, let's add a keyboard
+            // short listener to trigger it
+            if (model.get('type') === 'save') 
+            {
+                Tinycar.System.addEvent('ctrl+s', () => 
+                {
+                    container.trigger('click');
+                });
+            }
+            
+            return container;
+        }
+        
+        // Build list item container
+        private buildListItemContainer(model:Tinycar.Model.DataItem):JQuery
+        {
+            // Create container
+            let container = $('<a>').
+                attr('class', 'item').
+                attr('tabindex', 0);
+        
+            // Add link to internal path
             if (!model.hasString('service') && model.isObject('link_path'))
-			{
-				let path = Tinycar.Url.getWithVars(
-					model.get('link_path'), 
-					{url:Tinycar.Url.getParams()}
-				);
+            {
+                let path = Tinycar.Url.getWithVars(
+                    model.get('link_path'), 
+                    {url:Tinycar.Url.getParams()}
+                );
 
-				// Set link target
-				container.attr('href', Tinycar.Url.getAsPath(path));
-				
+                // Set link target
+                container.attr('href', Tinycar.Url.getAsPath(path));
+                
                 // Check if path matches, with fuzzy check
                 if (Tinycar.Url.isPathMatch(path, true))
                     container.addClass('is-active');
-			}
-			// Add link to API service
-			else if (model.hasString('link_service'))
-			{
+            }
+            // Add link to API service
+            else if (model.hasString('link_service'))
+            {
                 container.attr('href', Tinycar.Api.getServiceLink(
                     model.get('link_service')
                 ));
-			}
+            }
             
             // Add link target
             if (model.hasString('link_target'))
                 container.attr('target', model.get('link_target'));
-		
-			return container;
-		}
-		
-		// Build list item icon
-		private buildListItemIcon(model:Tinycar.Model.DataItem):JQuery
-		{
-			// Create container
-			let container = $('<span>').
-				attr('class', 'icon icon-small icon-lite icon-' + model.get('icon'));
-			
-			return container;
-		}
-		
-		// Build list item lable
-		private buildListItemLabel(model:Tinycar.Model.DataItem):JQuery
-		{
-			// Label string
-			let label = model.get('label');
-			
-			// This action opens a dialog
-			if (model.hasString('dialog'))
-				label += '...';
-			
-			// Create container
-			let container = $('<span>').
-				attr('class', 'label').
-				text(label);
-			
-			// Add label icon
-			$('<span>').
-				attr('class', 'icon icon-small icon-' + model.get('icon')).
-				appendTo(container);
-			
-			return container;
-		}
-		
-		// Build title
-		private buildTitle():void
-		{
-		    // Add title
-		    $('<div>').
-		        attr('class', 'title theme-text-liter').
-		        text(this.Model.get('title')).
-		        appendTo(this.htmlRoot);
-		    
-		    // Update root style
-		    this.htmlRoot.addClass('with-title');
-		}
-		
-		// @see Tinycar.Main.Component.setAsVisible()
-		setAsVisible(visible:boolean):void
-		{
-			// Update visibility only when we have list items
-			if (this.Model.getList('options').length > 0)
-				super.setAsVisible(visible);
-		}
-		
-		// Trigger list item execution
-		private triggerListItemAction(model:Tinycar.Model.DataItem):boolean
-		{
-			// Open dialog
-			if (model.hasString('dialog'))
-			{
-				this.App.openDialog(model.get('dialog'));
-				return true;
-			}
-			
-			// Save action
-			if (model.get('type') === 'save')
-			{
-				this.View.onSave(new Tinycar.Model.DataItem({
-					link_path : model.get('link_path'),
-					toast     : model.get('toast')
-				}));
-				
-				return true;
-			}
-			
-			// Expand or collapse list
-			if (model.get('type') === 'list')
-			{
-				this.View.App.toggleSideList();
-				return true;
-			}
-			
-			// Call custom service
-			if (model.hasString('service'))
-			{
-				this.View.callAction(model);
-				return true;
-			}
-			
-			return false;
-		}
-	}
+        
+            return container;
+        }
+        
+        // Build list item icon
+        private buildListItemIcon(model:Tinycar.Model.DataItem):JQuery
+        {
+            // Create container
+            let container = $('<span>').
+                attr('class', 'icon icon-small icon-lite icon-' + model.get('icon'));
+            
+            return container;
+        }
+        
+        // Build list item lable
+        private buildListItemLabel(model:Tinycar.Model.DataItem):JQuery
+        {
+            // Label string
+            let label = model.get('label');
+            
+            // This action opens a dialog
+            if (model.hasString('dialog'))
+                label += '...';
+            
+            // Create container
+            let container = $('<span>').
+                attr('class', 'label').
+                text(label);
+            
+            // Add label icon
+            $('<span>').
+                attr('class', 'icon icon-small icon-' + model.get('icon')).
+                appendTo(container);
+            
+            return container;
+        }
+        
+        // Build title
+        private buildTitle():void
+        {
+            // Add title
+            $('<div>').
+                attr('class', 'title theme-text-liter').
+                text(this.Model.get('title')).
+                appendTo(this.htmlRoot);
+            
+            // Update root style
+            this.htmlRoot.addClass('with-title');
+        }
+        
+        // @see Tinycar.Main.Component.setAsVisible()
+        setAsVisible(visible:boolean):void
+        {
+            // Update visibility only when we have list items
+            if (this.Model.getList('options').length > 0)
+                super.setAsVisible(visible);
+        }
+        
+        // Trigger list item execution
+        private triggerListItemAction(model:Tinycar.Model.DataItem):boolean
+        {
+            // Open dialog
+            if (model.hasString('dialog'))
+            {
+                this.App.openDialog(model.get('dialog'));
+                return true;
+            }
+            
+            // Save action
+            if (model.get('type') === 'save')
+            {
+                this.View.onSave(new Tinycar.Model.DataItem({
+                    link_path : model.get('link_path'),
+                    toast     : model.get('toast')
+                }));
+                
+                return true;
+            }
+            
+            // Expand or collapse list
+            if (model.get('type') === 'list')
+            {
+                this.View.App.toggleSideList();
+                return true;
+            }
+            
+            // Call custom service
+            if (model.hasString('service'))
+            {
+                this.View.callAction(model);
+                return true;
+            }
+            
+            return false;
+        }
+    }
 }
--- typescript/Tinycar/View/Tabs.ts
+++ typescript/Tinycar/View/Tabs.ts
@@ -1,151 +1,152 @@
 module Tinycar.View
 {
-	interface ITabItem
-	{
-		name:string;
-		label:string;
-	}
+    interface ITabItem
+    {
+        name:string;
+        label:string;
+    }
 
-	interface IHtmlTabs
-	{
-		[key:string]:JQuery;
-	}
+    interface IHtmlTabs
+    {
+        [key:string]:JQuery;
+    }
 
-	export class Tabs
-	{
-		private activeTab:string;
-		private htmlRoot:JQuery;
-		private htmlTabs:IHtmlTabs = {};
-		private tabList:Array<Object> = [];
-		private tabNames:Array<string> = [];
-		private View:Tinycar.Ui.View;
+    export class Tabs
+    {
+        private activeTab:string;
+        private htmlRoot:JQuery;
+        private htmlTabs:IHtmlTabs = {};
+        private tabList:Array<Object> = [];
+        private tabNames:Array<string> = [];
+        private View:Tinycar.Ui.View;
 
 
-		// Initiate class
-		constructor(view:Tinycar.Ui.View)
-		{
-			this.View = view;
-		}
+        // Initiate class
+        constructor(view:Tinycar.Ui.View)
+        {
+            this.View = view;
+        }
 
-		// Add new tab item
-		addItem(item:ITabItem):void
-		{
-			this.tabList.push(item);
-		}
+        // Add new tab item
+        addItem(item:ITabItem):void
+        {
+            this.tabList.push(item);
+        }
 
-		// Build
-		build():JQuery
-		{
-			// Build elements
-			this.buildRoot();
-			this.buildLine();
+        // Build
+        build():JQuery
+        {
+            // Build elements
+            this.buildRoot();
+            this.buildLine();
 
-			// Build tabs
-			this.tabList.forEach((item:Object, index:number) =>
-			{
-				this.buildItem(item, index);
-			});
+            // Build tabs
+            this.tabList.forEach((item:Object, index:number) =>
+            {
+                this.buildItem(item, index);
+            });
 
-			return this.htmlRoot;
-		}
+            return this.htmlRoot;
+        }
 
-		// Build new tab item
-		private buildItem(item:Object, index:number):void
-		{
-			// Create link container
-			let container = $('<a>').
-				attr('class', 'item').
-				attr('tabindex', 0).
-				text(item['label']).
-				appendTo(this.htmlRoot);
+        // Build new tab item
+        private buildItem(item:Object, index:number):void
+        {
+            // Create link container
+            let container = $('<a>').
+                attr('class', 'item').
+                attr('tabindex', 0).
+                text(item['label']).
+                appendTo(this.htmlRoot);
+            
+            // When clicked
+            container.click((e:Event) =>
+            {
+                e.preventDefault();
+                container.blur();
+                this.View.showTab(item['name']);
+            });
 
-			// When clicked
-			container.click((e:Event) =>
-			{
-				e.preventDefault();
-				this.View.showTab(item['name']);
-			});
+            // When key is pressed
+            container.keydown((e:JQueryKeyEventObject) =>
+            {
+                // Open dialog when space bar or Enter is pressed
+                if (e.which === 32 || e.which === 13)
+                {
+                    e.preventDefault();
+                    this.View.showTab(item['name']);
+                }
+                // Focus to previous tab link
+                else if (e.which === 37 && index !== 0)
+                {
+                    e.preventDefault();
+                    $(e.target).prev().focus();
+                }
+                // Focus to next tab link
+                else if (e.which === 39 && index !== this.tabNames.length - 1)
+                {
+                    e.preventDefault();
+                    $(e.target).next().focus();
+                }
+            });
 
-			// When key is pressed
-			container.keydown((e:JQueryKeyEventObject) =>
-			{
-				// Open dialog when space bar or Enter is pressed
-				if (e.which === 32 || e.which === 13)
-				{
-					e.preventDefault();
-					container.click();
-				}
-				// Focus to previous tab link
-				else if (e.which === 37 && index !== 0)
-				{
-					e.preventDefault();
-					$(e.target).prev().focus();
-				}
-				// Focus to next tab link
-				else if (e.which === 39 && index !== this.tabNames.length - 1)
-				{
-					e.preventDefault();
-					$(e.target).next().focus();
-				}
-			});
+            // Add name to list
+            this.tabNames.push(item['name']);
 
-			// Add name to list
-			this.tabNames.push(item['name']);
+            // Remember instance
+            this.htmlTabs[item['name']] = container;
+        }
 
-			// Remember instance
-			this.htmlTabs[item['name']] = container;
-		}
+        // Build lien
+        private buildLine():void
+        {
+            $('<span>').
+                attr('class', 'line').
+                appendTo(this.htmlRoot);
+        }
 
-		// Build lien
-		private buildLine():void
-		{
-			$('<span>').
-				attr('class', 'line').
-				appendTo(this.htmlRoot);
-		}
+        // Build root container
+        private buildRoot():void
+        {
+            this.htmlRoot = $('<div>').
+                attr('class', 'tinycar-view-tabs');
+        }
 
-		// Build root container
-		private buildRoot():void
-		{
-			this.htmlRoot = $('<div>').
-				attr('class', 'tinycar-view-tabs');
-		}
+        // Get first available tab name
+        getFirstTab():string
+        {
+            return (this.tabNames.length > 0 ?
+                this.tabNames[0] : 'default'
+            );
+        }
 
-		// Get first available tab name
-		getFirstTab():string
-		{
-			return (this.tabNames.length > 0 ?
-				this.tabNames[0] : 'default'
-			);
-		}
+        // Get current active name
+        getActiveTab():string
+        {
+            return this.activeTab;
+        }
 
-		// Get current active name
-		getActiveTab():string
-		{
-			return this.activeTab;
-		}
+        // Set specified tab name as active
+        setActiveTab(name:string):void
+        {
+            // Unselect current tab
+            if (typeof this.activeTab === 'string')
+            {
+                this.htmlTabs[this.activeTab].
+                    removeClass('is-active').
+                    removeClass('theme-border');
+            }
 
-		// Set specified tab name as active
-		setActiveTab(name:string):void
-		{
-			// Unselect current tab
-			if (typeof this.activeTab === 'string')
-			{
-				this.htmlTabs[this.activeTab].
-					removeClass('is-active').
-					removeClass('theme-border');
-			}
+            // Remember
+            this.activeTab = name;
 
-			// Remember
-			this.activeTab = name;
-
-			// Select new tab
-			if (this.htmlTabs.hasOwnProperty(name))
-			{
-				this.htmlTabs[this.activeTab].
-					addClass('is-active').
-					addClass('theme-border');
-			}
-		}
-	}
+            // Select new tab
+            if (this.htmlTabs.hasOwnProperty(name))
+            {
+                this.htmlTabs[this.activeTab].
+                    addClass('is-active').
+                    addClass('theme-border');
+            }
+        }
+    }
 }
