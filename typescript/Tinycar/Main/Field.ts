44e2edda1938714a01ee85905b4118b16d0dc1c3
Antti Launiainen <antti.launiainen@suomi24.fi>
2016-05-05 10:56:40 -0400

    Support disabled state for SelectList

--- typescript/Tinycar/Main/Field.ts
+++ typescript/Tinycar/Main/Field.ts
@@ -1,153 +1,202 @@
 module Tinycar.Main
 {
-	import m = Tinycar.Model;
-	import u = Tinycar.Ui;
-	
-	export class Field extends Tinycar.Main.Component
-	{
-		htmlContent:JQuery;
-		
-		// Build
-		build():JQuery
-		{
-			// Build elements
-			this.buildRoot();
-			
-			// Build type label
-			if (this.Model.hasString('type_label'))
-				this.buildTypeLabel();
-			else
-				this.buildEmptyLabel();
-			
-			this.buildContent();
-			
-			// Build type instructions
-			if (this.Model.hasString('type_instructions'))
-				this.buildTypeInstructions();
-			
-			return this.htmlRoot;
-		}
-		
-		// @see Tinycar.Main.Component.buildContent()
-		buildContent():void
-		{
-			// Create container
-			this.htmlContent = $('<div>').
-				attr('class', 'type-content').
-				appendTo(this.htmlRoot);
-		}
-		
-		// Build empty label
-		private buildEmptyLabel():void
-		{
-			$('<div>').
-				attr('class', 'type-label-empty').
-				appendTo(this.htmlRoot);
-		}
-		
-		// Build type instructions
-		private buildTypeInstructions():void
-		{
-			$('<div>').
-				attr('class', 'type-instructions').
-				text(this.Model.get('type_instructions')).
-				appendTo(this.htmlContent);
-		}
-		
-		// Build type label
-		private buildTypeLabel():void
-		{
-			// Create container
-			let container = $('<div>').
-				attr('class', 'type-label').
-				appendTo(this.htmlRoot);
-			
-			// Create label
-			let label = $('<label>').
-				attr('class', 'label').
-				attr('for', this.getFieldId()).
-				text(this.Model.get('type_label')).
-				appendTo(container);
-			
-			// This is a required field
-			if (this.Model.get('data_required') === true)
-			{
-				// Add label title
-				label.attr('title', 
-					Tinycar.Locale.getText('info_required_field')
-				);
-				
-				// Add required mark
-				$('<span>').
-					attr('class', 'required theme-base').
-					appendTo(label);
-			}
-			
-			// This field has a help text
-			if (this.Model.hasString('type_help'))
-			{
-				let help = new Tinycar.Ui.Main.Help(this.Model.get('type_help'));
-				container.append(help.build());
-			}
-		}
-		
-		// Set focus to this field
-		focus():void
-		{
-			// @note: implement in derived class
-		}
-		
-		// @see Tinycar.Main.Component.getRootStyles()
-		getRootStyles():Array<string>
-		{
-			let result = super.getRootStyles();
-			
-			// Base class
-			result.push('tinycar-main-field');
-			
-			// This field is not disabled
-			if (this.isTypeEnabled() === true)
-				result.push('is-enabled');
-			
-			return result;
-		}
-		
-		// Get current component's data name
-		getDataName():string
-		{
-			return this.Model.get('data_name'); 
-		}
-		
-		// Get current component's data value
-		getDataValue():any
-		{
-			return this.Model.get('data_value');
-		}
-		
-		// Get unique field id
-		getFieldId(postfix:string = ''):string
-		{
-			return 'field-' + this.Model.get('id') + '-' + postfix;
-		}
-		
-		// Check if this field is enabled
-		isTypeEnabled():boolean
-		{
-			return (this.Model.get('type_enabled') !== false);
-		}
-		
-		// Set new data value
-		setDataValue(value:any):void
-		{
-		    // This is a new value
-		    if (this.Model.get('data_value') !== value)
-		    {
-		        // Set new value
-	            this.Model.set('data_value', value);
-	            
-	            // Trigger bindings
-		        this.View.triggerBindSource(this);
-		    }
-		}
-	}
+    import m = Tinycar.Model;
+    import u = Tinycar.Ui;
+    
+    export class Field extends Tinycar.Main.Component
+    {
+        htmlContent:JQuery;
+        private isEnabled:boolean = false;
+        
+        // Build
+        build():JQuery
+        {
+            // Build elements
+            this.buildRoot();
+            
+            // Build type label
+            if (this.Model.hasString('type_label'))
+                this.buildTypeLabel();
+            else
+                this.buildEmptyLabel();
+            
+            this.buildContent();
+            
+            // Build type instructions
+            if (this.Model.hasString('type_instructions'))
+                this.buildTypeInstructions();
+            
+            return this.htmlRoot;
+        }
+        
+        // @see Tinycar.Main.Component.buildContent()
+        buildContent():void
+        {
+            // Create container
+            this.htmlContent = $('<div>').
+                attr('class', 'type-content').
+                appendTo(this.htmlRoot);
+        }
+        
+        // Build empty label
+        private buildEmptyLabel():void
+        {
+            $('<div>').
+                attr('class', 'type-label-empty').
+                appendTo(this.htmlRoot);
+        }
+        
+        // Build type instructions
+        private buildTypeInstructions():void
+        {
+            $('<div>').
+                attr('class', 'type-instructions').
+                text(this.Model.get('type_instructions')).
+                appendTo(this.htmlContent);
+        }
+        
+        // Build type label
+        private buildTypeLabel():void
+        {
+            // Create container
+            let container = $('<div>').
+                attr('class', 'type-label').
+                appendTo(this.htmlRoot);
+            
+            // Create label
+            let label = $('<label>').
+                attr('class', 'label').
+                attr('for', this.getFieldId()).
+                text(this.Model.get('type_label')).
+                appendTo(container);
+            
+            // This is a required field
+            if (this.Model.get('data_required') === true)
+            {
+                // Add label title
+                label.attr('title', 
+                    Tinycar.Locale.getText('info_required_field')
+                );
+                
+                // Add required mark
+                $('<span>').
+                    attr('class', 'required theme-base').
+                    appendTo(label);
+            }
+            
+            // This field has a help text
+            if (this.Model.hasString('type_help'))
+            {
+                let help = new Tinycar.Ui.Main.Help(this.Model.get('type_help'));
+                container.append(help.build());
+            }
+        }
+        
+        // Set focus to this field
+        focus():void
+        {
+            // @note: implement in derived class
+        }
+        
+        // @see Tinycar.Main.Component.getRootStyles()
+        getRootStyles():Array<string>
+        {
+            let result = super.getRootStyles();
+            
+            // Base class
+            result.push('tinycar-main-field');
+            
+            return result;
+        }
+        
+        // Get current component's data name
+        getDataName():string
+        {
+            return this.Model.get('data_name'); 
+        }
+        
+        // Get current component's data value
+        getDataValue():any
+        {
+            return this.Model.get('data_value');
+        }
+        
+        // Get unique field id
+        getFieldId(postfix:string = ''):string
+        {
+            return 'field-' + this.Model.get('id') + '-' + postfix;
+        }
+        
+        // Check if this field has a dataname
+        hasDataName():boolean
+        {
+            return this.Model.isString('data_name');
+        }
+        
+        // Check if this field is enabled
+        isTypeEnabled():boolean
+        {
+            return (this.Model.get('type_enabled') !== false);
+        }
+        
+        // Set field as enabled
+        setAsEnabled(status:boolean):boolean
+        {
+            // Enable field
+            if (status && !this.isEnabled)
+            {
+                this.isEnabled = true;
+                this.htmlRoot.addClass('is-enabled');
+                return true;
+            }
+            
+            // Disable field
+            if (!status && this.isEnabled)
+            {
+                this.isEnabled = false;
+                this.htmlRoot.removeClass('is-enabled');
+                return true;
+            }
+            
+            // State did not change
+            return false;
+        }
+        
+        // Set new data value
+        setDataValue(value:any):void
+        {
+            // This is a new value
+            if (this.Model.get('data_value') !== value)
+            {
+                // Set new value
+                this.Model.set('data_value', value);
+                
+                // We must have a name to trigger binding
+                if (this.hasDataName())
+                {
+                    // Update binding property value
+                    this.View.Bind.set(
+                        this.Model.get('data_name'), 
+                        this.Model.get('data_value')
+                    );
+                    
+                    // Trigger bindings
+                    this.View.triggerBinding(
+                        this.Model.get('data_name') 
+                    );
+                }
+            }
+        }
+        
+        // @see Tinycar.Main.Component 
+        start():void
+        {
+            // Fields that support disabled state should be set
+            // disabled by defualt, so set them only as 
+            // enabled when initiated
+            
+            if (this.isTypeEnabled())
+                this.setAsEnabled(true);
+        }
+    }
 }
--- typescript/Tinycar/Ui/Component/SelectList.ts
+++ typescript/Tinycar/Ui/Component/SelectList.ts
@@ -1,70 +1,83 @@
 module Tinycar.Ui.Component
 {
-	export class SelectList extends Tinycar.Main.Field
-	{
-		private fldList:JQuery;
-	
-	
-		// Build content
-		buildContent()
-		{
-			// Build elements
-			super.buildContent();
-			this.buildList();
-		}
-		
-		// Build textarea field
-		private buildList():void
-		{
-			// Create container
-			let container = $('<div>').
-				attr('class', 'list').
-				appendTo(this.htmlContent);
-			
-			// Add arrow symbol
-			$('<span>').
-				attr('class', 'icon icon-small icon-sort-down').
-				appendTo(container);
-			
-			// Add select menu
-			this.fldList = $('<select>').
-				attr('size', 1).
-				appendTo(container);
-			
-			// Get options
-			let options = this.Model.get('options');
-			
-			// Create options
-			options.forEach((item:Object) =>
-			{
-				$('<option>').
-					attr('value', item['name']).
-					text(item['label']).
-					appendTo(this.fldList);
-			});
-				
-			// When value is changed
-			this.fldList.change((e:Event) =>
-			{
-				this.setDataValue(this.fldList.val());
-			});
+    export class SelectList extends Tinycar.Main.Field
+    {
+        private fldList:JQuery;
+    
+    
+        // @see Tinycar.Main.Field.build()
+        buildContent()
+        {
+            // Build elements
+            super.buildContent();
+            this.buildList();
+        }
+        
+        // Build textarea field
+        private buildList():void
+        {
+            // Create container
+            let container = $('<div>').
+                attr('class', 'list').
+                appendTo(this.htmlContent);
+            
+            // Add arrow symbol
+            $('<span>').
+                attr('class', 'icon icon-small icon-sort-down').
+                appendTo(container);
+            
+            // Add select menu
+            this.fldList = $('<select>').
+                attr('size', 1).
+                attr('disabled', 'disabled').
+                appendTo(container);
+            
+            // Get options
+            let options = this.Model.get('options');
+            
+            // Create options
+            options.forEach((item:Object) =>
+            {
+                $('<option>').
+                    attr('value', item['name']).
+                    text(item['label']).
+                    appendTo(this.fldList);
+            });
+                
+            // When value is changed
+            this.fldList.change((e:Event) =>
+            {
+                this.setDataValue(this.fldList.val());
+            });
 
-			// No initial value, select first option
-			if (this.Model.get('data_value') === null)
-			{
-				if (options.length > 0)
-					this.Model.set('data_value', options[0]['name']);
-			}
-			
-			// Select initial custom value
-			if (this.Model.get('data_value') !== null)
-				this.fldList.val(this.Model.get('data_value'));
-		}
-		
-		// @see Tinycar.Main.Field.focus()
-		focus():void
-		{
-			this.fldList.focus();
-		}
-	}
+            // No initial value, select first option
+            if (this.Model.get('data_value') === null)
+            {
+                if (options.length > 0)
+                    this.Model.set('data_value', options[0]['name']);
+            }
+            
+            // Select initial custom value
+            if (this.Model.get('data_value') !== null)
+                this.fldList.val(this.Model.get('data_value'));
+        }
+        
+        // @see Tinycar.Main.Field.focus()
+        focus():void
+        {
+            this.fldList.focus();
+        }
+        
+        // @see Tinycar.Main.Field.setAsEnabled()
+        setAsEnabled(status:boolean):boolean
+        {
+            // State did not change 
+            if (!super.setAsEnabled(status))
+                return false;
+            
+            // Update field status
+            this.fldList.prop('disabled', !status);
+            return true;
+        }
+    }
 }
