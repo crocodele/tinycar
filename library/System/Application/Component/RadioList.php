058f900c6a504dae0bba72032194dd55a885e37e
Antti Launiainen <antti.launiainen@suomi24.fi>
2016-05-07 08:22:28 -0400

    RadioList was not typed as Field and was missing type_label

--- library/System/Application/Component/RadioList.php
+++ library/System/Application/Component/RadioList.php
@@ -1,68 +1,66 @@
 <?php
-	
-	namespace Tinycar\System\Application\Component;
 
-	use Tinycar\Core\Http\Params;
-	use Tinycar\System\Application\Component;
-	
-	class RadioList extends Component
-	{
-		
-		/**
-		 * Get select list options data
-		 * @return array options data
-		 */
-		private function getOptionsData()
-		{
-			$result = array();
-			
-			// Go trough options
-			foreach ($this->xdata->getNodes('options/option') as $node)
-			{
-				// Ddefaults
-				$item = array(
-					'name'         => $node->getString('@name'),
-					'label'        => $node->getString('@label'),
-					'help'         => $node->getString('help'),
-					'instructions' => $node->getString('instructions'),
-				);
-				
-				// Translate label
-				$item['label'] = $this->getStringValue(
-					$item['label']
-				);
-				
-				// Translate help
-				$item['help'] = $this->getStringValue(
-					$item['help']
-				);
-				
-				// Translate instructions
-				$item['instructions'] = $this->getStringValue(
-					$item['instructions']
-				);
-				
-				// Add to list
-				$result[] = $item;
-			}
-			
-			return $result;
-		}
-		
-		
-		/**
-		 * @see Tinycar\System\Application\Compont::onModelAction()
-		 */
-		public function onModelAction(Params $params)
-		{
-			$result = parent::onModelAction($params);
-			
-			// Layout style
-			$result['layout'] = $this->xdata->getString('layout');
+    namespace Tinycar\System\Application\Component;
 
-			// Get options data
-			$result['options'] = $this->getOptionsData();
-			
-			return $result;
-		}
-	}
+    use Tinycar\Core\Http\Params;
+    use Tinycar\System\Application\View\Field;
+
+    class RadioList extends Field
+    {
+
+        /**
+         * Get select list options data
+         * @return array options data
+         */
+        private function getOptionsData()
+        {
+            $result = array();
+
+            // Go trough options
+            foreach ($this->xdata->getNodes('options/option') as $node)
+            {
+                // Ddefaults
+                $item = array(
+                    'name'         => $node->getString('@name'),
+                    'label'        => $node->getString('@label'),
+                    'help'         => $node->getString('help'),
+                    'instructions' => $node->getString('instructions'),
+                );
+
+                // Translate label
+                $item['label'] = $this->getStringValue(
+                    $item['label']
+                );
+
+                // Translate help
+                $item['help'] = $this->getStringValue(
+                    $item['help']
+                );
+
+                // Translate instructions
+                $item['instructions'] = $this->getStringValue(
+                    $item['instructions']
+                );
+
+                // Add to list
+                $result[] = $item;
+            }
+
+            return $result;
+        }
+
+
+        /**
+         * @see Tinycar\System\Application\View\Field::onModelAction()
+         */
+        public function onModelAction(Params $params)
+        {
+            $result = parent::onModelAction($params);
+
+            // Add properties
+            $result['layout']  = $this->getNodeString('layout');
+            $result['options'] = $this->getOptionsData();
+
+            return $result;
+        }
+    }
